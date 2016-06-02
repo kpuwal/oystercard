32610f89b897251e578059fbb78e4550c0c52da7
kpuwal <kpuwal@gmail.com>
2016-06-02 14:53:02 +0100

    working on penalty method in journey class

--- lib/journey.rb
+++ lib/journey.rb
@@ -1,11 +1,14 @@
 class Journey
 
 	attr_reader :entry_station, :exit_station, :in_journey
+      PENALTY_FARE= 6
+      MINIMUM_FARE = 1
 
 	def initialize
-		@entry_station 
+		@entry_station
 		@exit_station
 		@in_journey = false
+              @fare= MINIMUM_FARE
 	end
 
 	def start(station_in)
@@ -17,5 +20,21 @@ class Journey
 		@exit_station = station_out
 		@in_journey = false
 	end
+
+      def fare
+          @fare = MINIMUM_FARE
+      end
+
+      def penalty_fare
+        if entry_station == nil || exit_station == nil
+        @fare= PENALTY_FARE
+        else
+        @fare
+        end
+      end
+
+      def fresh
+        @entry_station = nil
+      end
 
 end
--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -31,5 +31,35 @@ describe Journey do
 		end
 
 	end
-	
+
+      describe '#fare' do
+        it 'returns a minimum fare' do
+          expect(subject.fare).to eq Journey::MINIMUM_FARE
+        end
+      end
+
+      describe '#penalty_fare' do
+        it 'has a penalty fare of 6' do
+          #subject.end(station_out)
+          expect(subject.penalty_fare).to eq 6
+        end
+
+        it 'returns penalty fare if there is no entry station' do
+          #subject.fresh
+          #subject.end(station_out)
+
+          #expect(subject.fare).to eq Journey::PENALTY_FARE
+      end
+      end	
+
+      describe '#fresh' do
+        it 'clears entry station' do
+          subject.fresh
+          expect(subject.entry_station).to eq nil
+      end
+
+      it 'clears exit station' do
+          expect(subject.exit_station).to eq nil
+        end
+      end
 end
